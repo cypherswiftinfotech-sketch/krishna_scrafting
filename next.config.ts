d3d45d7bac80c8aed28e7b8754c26a9bd5f2e9e4
Sagararya07 <sagarsagar49061@email.com>
2026-06-29 13:58:53 +0530

    fix: Add pg and jsonwebtoken to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  serverExternalPackages: ["bcryptjs"],
+  serverExternalPackages: ["bcryptjs", "pg", "jsonwebtoken"],
 };
 
 export default nextConfig;
